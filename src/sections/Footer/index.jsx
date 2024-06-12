881af2a081376c89b9b114b1f3f80860e6ecc50c
Anirudh Dabral <anirudhdabral49@gmail.com>
2024-06-12 11:38:09 +0530

    first commit

---
+++ src/sections/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './index.module.css'
+
+export const Footer = () => {
+    return (
+        <section id="footer" className={styles.container}>
+          <p>
+            &copy; 2024 Harris johnsen. <br />
+            All rights reserved.
+          </p>
+        </section>
+      );
+}
---
+++ src/sections/Projects/index.jsx
@@ -0,0 +1,40 @@
+import styles from './index.module.css';
+import kanban from '../../assets/taskstream.jpeg';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+
+export const Projects = () => {
+    return (
+        <section id="projects" className={styles.container}>
+          <h1 className="sectionTitle">Projects</h1>
+          <div className={styles.projectsContainer}>
+            <ProjectCard
+              src={kanban}
+              link="https://kanban-app-anirudhdabral.web.app/"
+              h3="Taskstream"
+              p="Kanban Board"
+            />
+            <ProjectCard
+              src={freshBurger}
+              link="https://github.com/Ade-mir/company-landing-page-2"
+              h3="Fresh Burger"
+              p="Hamburger Restaurant"
+            />
+            <ProjectCard
+              src={hipsster}
+              link="https://github.com/Ade-mir/company-landing-page-2"
+              h3="Hipsster"
+              p="Glasses Shop"
+            />
+            <ProjectCard
+              src={fitLift}
+              link="https://github.com/Ade-mir/company-landing-page-2"
+              h3="FitLift"
+              p="Fitness App"
+            />
+          </div>
+        </section>
+      );
+}
